9a45d39ab631c1b11a5313d6d4beccfe6c513b54
Andrii Torkotiuk <torkotiuk@gmail.com>
2020-12-09 23:25:15 +0100

    task 5 corrected

--- js/modul-2/task-5.js
+++ js/modul-2/task-5.js
@@ -1,9 +1,14 @@
 const checkForSpam = function(message) {
   const newMessage = message.toLocaleLowerCase();
-  console.log(newMessage.includes('spam') || newMessage.includes('sale'));
+  // console.log(newMessage.includes('spam') || newMessage.includes('sale'));
+  if (true) {
+    return newMessage.includes('spam') || newMessage.includes('sale');
+  } else {
+    return;
+  }
 }
 
-checkForSpam('Latest technology news'); // false
-checkForSpam('JavaScript weekly newsletter'); // false
-checkForSpam('Get best sale offers now!'); // true
-checkForSpam('[SPAM] How to earn fast money?'); // true
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
